fcd38b230f1c2b328dec3a63a21da8785e8a1bf1
Yauheni Kiryienkau <ya@redstream.by>
2024-01-30 16:16:18 +0300

    moved to stable removed redundant params

--- src/Attributes/Controller.php
+++ src/Attributes/Controller.php
@@ -7,14 +7,4 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS)]
 class Controller {
 
-	protected ?string $title = null;
-
-	public function __construct(?string $title = null) {
-		$this->title = $title;
-	}
-
-	public function getTitle(): ?string {
-		return $this->title;
-	}
-
 }
--- src/Attributes/Method.php
+++ src/Attributes/Method.php
@@ -7,18 +7,12 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 class Method {
 
-	protected ?string $title = null;
 	protected ?string $request = null;
 	protected ?string $response = null;
 
-	public function __construct(?string $title = null, ?string $request = null, ?string $response = null) {
-		$this->title = $title;
+	public function __construct(?string $request = null, ?string $response = null) {
 		$this->request = $request;
 		$this->response = $response;
-	}
-
-	public function getTitle(): ?string {
-		return $this->title;
 	}
 
 	public function getRequest(): ?string {
